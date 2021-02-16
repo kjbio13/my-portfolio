e2965b360e36c93fca834906a68678d37cce67cd
Kevin Souribio (Personal) <kevinsouribio@nandos-ltmb-kev.local>
2021-02-16 00:56:51 +0000

    refactor app file

--- src/components/design-projects/design.data.js
+++ src/components/design-projects/design.data.js
@@ -6,45 +6,45 @@ const DESIGN_DATA = [
         descriptionText: "Games Development Company",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     },
     {
         id: 2,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b7ae3c101743433.5f2731df9c194.png",
         descriptionTitle: "black & white",
         descriptionText: "Men's Suits Store",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     } 
     ,
     {
         id: 3,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e39379101743433.5f260eec1c852.png",
         descriptionTitle: "Malero",
         descriptionText: "Driverless Car Company",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     }
     ,
     {
         id: 4,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6b8088101743433.5f260eec1a3cd.png",
         descriptionTitle: "Fleur",
         descriptionText: "Flower Shop",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     }
     ,
     {
         id: 5,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/3650d2101743433.5f260eec184e7.png",
         descriptionTitle: "Original East London Cafe",
         descriptionText: "Coffee Shop",
         buttonText1: "Branding",
@@ -55,34 +55,34 @@ const DESIGN_DATA = [
     ,
     {
         id: 6,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/44d321101743433.5f260eec1adc9.png",
         descriptionTitle: "Snowfall",
         descriptionText: "Ski Mountain Hire Company",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     }
     ,
     {
         id: 7,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2d643d101743433.5f260eec1d036.png",
         descriptionTitle: "Ember",
         descriptionText: "High-End Meat Grill Restaurant",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     }
     ,
     {
         id: 8,
-        imageUrl: "https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/43252799873991.5efc750c3a39e.png",
-        descriptionTitle: "Frostees",
+        imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/41c699101743433.5f260eec1bf65.png",
+        descriptionTitle: "Frostee",
         descriptionText: "Cupcake Bakery",
         buttonText1: "Branding",
         buttonText2: "UI/UX",
-        buttonUrl1: "https://www.behance.net/gallery/97562563/NOVA",
+        buttonUrl1: "https://www.behance.net/kevinjsouribi",
         buttonUrl2: "https://dribbble.com/"
     }
 
