1f6519eb432b1dd101b1f86fbc438c786a46522e
Kevin Souribio (Personal) <kevinsouribio@nandos-ltmb-kev.local>
2021-02-16 03:58:10 +0000

    Updates

---
+++ precache-manifest.25fe7f07bc33a13ebaf04a07b81f2e06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9cba84483a8125a250bca3bb854328",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "fb93b0c30b38a812a5ec",
+    "url": "/my-portfolio/static/css/main.aefc2f53.chunk.css"
+  },
+  {
+    "revision": "c528164888d99aa50536",
+    "url": "/my-portfolio/static/js/2.4db7d5aa.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/my-portfolio/static/js/2.4db7d5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb93b0c30b38a812a5ec",
+    "url": "/my-portfolio/static/js/main.b56fec9d.chunk.js"
+  },
+  {
+    "revision": "2f1bd77cb6936f293c5b",
+    "url": "/my-portfolio/static/js/runtime-main.101a0773.js"
+  },
+  {
+    "revision": "4d8191aa478c5870be656139c2c7a513",
+    "url": "/my-portfolio/static/media/K.4d8191aa.svg"
+  },
+  {
+    "revision": "535744fe595820ea822ee85131d79abd",
+    "url": "/my-portfolio/static/media/circle.535744fe.svg"
+  },
+  {
+    "revision": "e9a257ecf01b76a1246d75b14b88b9a2",
+    "url": "/my-portfolio/static/media/profile-photo.e9a257ec.png"
+  },
+  {
+    "revision": "838e673922142a527d57b29475eb9e0e",
+    "url": "/my-portfolio/static/media/resume.838e6739.pdf"
+  },
+  {
+    "revision": "37857ad3f846f65476cffb20d3dfc4e5",
+    "url": "/my-portfolio/static/media/star.37857ad3.svg"
+  }
+]);
